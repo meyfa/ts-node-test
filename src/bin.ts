25f0492ec168f0e9f4b2ac2c3aa670164e750ab0
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2022-12-26 16:38:42 +0100

    feat: Implement support for watch mode
    Fixes #28. This patch adds a dependency on `yargs` to be able to parse
    command-line arguments. If `--watch` is provided, it will be passed
    along to the spawned Node.js child process. As a side effect of using
    yargs, we now also support the commands `ts-node-test --version` and
    `ts-node-test --help`.

--- src/bin.ts
+++ src/bin.ts
@@ -1,5 +1,21 @@
 #!/usr/bin/env node
 
 import { main } from './index.js'
+import yargs from 'yargs'
 
-await main()
+const parsedArgs = yargs(process.argv.slice(2)).command('* <paths...>', 'Run tests', (yargs) => {
+  return yargs.options({
+    watch: {
+      type: 'boolean',
+      default: false,
+      description: 'Run in watch mode'
+    }
+  }).positional('paths', {
+    type: 'string',
+    array: true,
+    demandOption: true,
+    description: 'Paths to test files'
+  })
+}).parseSync()
+
+await main(parsedArgs.paths, parsedArgs)
